6cf4f2255f147ebe3746e865f649b7ad2971b209
Clement <clement.llobet@tutanota.com>
2023-04-13 10:37:44 +0200

    create new Models

---
+++ TheCarHub/Areas/Admin/Models/CarDetails.cs
@@ -0,0 +1,19 @@
+﻿namespace TheCarHub.Areas.Admin.Models
+{
+    public class CarDetails
+    {
+        public int Id { get; set; }
+        //public string VIN { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Trim { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public decimal Purchase { get; set; }
+        public string Repairs { get; set; }
+        public decimal RepairsCost { get; set;}
+        public DateTime LotDate { get; set; }
+        public decimal SellingPrice { get; set; }
+        public DateTime SaleDate { get; set; }
+    }
+}
---
+++ TheCarHub/Areas/Admin/Models/CarImage.cs
@@ -0,0 +1,8 @@
+﻿namespace TheCarHub.Areas.Admin.Models
+{
+    public class CarImage
+    {
+        public int Id { get; set; }
+        public string UrlImage { get; set; }
+    }
+}
