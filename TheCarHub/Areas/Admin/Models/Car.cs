f5a64d8efbef1453fe0e1602abed9335204e5034
Clement <clement.llobet@tutanota.com>
2023-04-13 10:27:13 +0200

    add Image to CarsController

--- TheCarHub/Areas/Admin/Models/Car.cs
+++ TheCarHub/Areas/Admin/Models/Car.cs
@@ -1,4 +1,7 @@
-﻿namespace TheCarHub.Areas.Admin.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheCarHub.Areas.Admin.Models
 {
     public class Car
     {
@@ -6,6 +9,8 @@
         public string Name { get; set; }
         public string UrlImage { get; set; }
         public string Description { get; set; }
+        [NotMapped]
+        public IFormFile Image { get; set; }
         public DateTime YearDate { get; set; }
         public decimal Price { get; set; }
         public bool IsAvailable { get; set; }
