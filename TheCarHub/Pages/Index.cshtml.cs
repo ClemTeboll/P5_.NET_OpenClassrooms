49a4bb68d4031773fbf6ebddacd189c8b6f43b3d
Clement <clement.llobet@tutanota.com>
2023-06-09 15:42:23 +0200

    Display CarDtoRead on Index page

--- TheCarHub/Pages/Index.cshtml.cs
+++ TheCarHub/Pages/Index.cshtml.cs
@@ -1,21 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-//using TheCarHub.Areas.Admin.DTO.Read.CarDtoRead;
+using Microsoft.EntityFrameworkCore;
+using TheCarHub.Areas.Admin.DTO.Read;
+using TheCarHub.Areas.Admin.Models;
+using TheCarHub.Data;
 
 namespace TheCarHub.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public IEnumerable<CarDtoRead> _carDtoReadList { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly ILogger<CarDtoRead> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public IndexModel(IWebHostEnvironment webHostEnvironment, ILogger<CarDtoRead> logger, ApplicationDbContext context, IMapper mapper)
         {
+            _webHostEnvironment = webHostEnvironment;
             _logger = logger;
+            _context = context;
+            _mapper = mapper;
         }
 
         public void OnGet()
         {
-            
+            List<CarDtoRead> dtoList = new List<CarDtoRead>();
+
+            foreach (Car car in _context.Car
+                .Where(x => x.IsAvailable)
+                .Include(y => y.CarImages)
+                .Include(z => z.CarDetails)
+                .Include(z => z.CarDetails.CarModel)
+                .AsNoTracking()
+                .ToList())
+            {
+                if (car.IsAvailable)
+                {
+                    var carObject = (car, car.CarImages.First(), car.CarDetails);
+                    CarDtoRead carDtoRead = _mapper.Map<CarDtoRead>(carObject);
+
+                    dtoList.Add(carDtoRead);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            _carDtoReadList = dtoList;
         }
     }
 }
